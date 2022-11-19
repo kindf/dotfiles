0e6f0ab37dd6f098998369b51234898ed2af602f
kindf <1304298803@qq.com>
2022-11-20 02:56:17 +0800

    添加python3.7安装脚本

---
+++ etc/python3.7_pkg_install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+# pkg安装neovim(~/.dotfiles/pkg)
+
+set -e
+set -x
+
+PROJECT_PATH=~/.dotfiles
+PKG_PATH=${PROJECT_PATH}/pkg
+mkdir -p ${PKG_PATH}
+cd ${PKG_PATH}
+
+VERSION=3.7.4
+PKG_NAME=Python-${VERSION}.tgz
+if [ ! -f ${PKG_NAME} ]; then
+    wget -O ${PKG_NAME} https://www.python.org/ftp/python/${VERSION}/${PKG_NAME}
+fi
+tar -zxvf ${PKG_NAME}
+
+INSTALL_PATN=${PKG_PATH}/Python-${VERSION}/
+cd ${INSTALL_PATN}
+./configure --prefix=${INSTALL_PATN}
+make
+
+# 支持pip
+if [ ! -f "get-pip.py" ]; then
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+fi
+
+./python get-pip.py
+./python -m pip install pynvim
+
+CUR_PATH=`pwd`
+ln -s ${CUR_PATH}/python ${PROJECT_PATH}/bin/python
+
+cd ../..
